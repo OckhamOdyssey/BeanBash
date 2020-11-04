9e3a329b178f5ea2090701213a4a91dad6cbf64b
TheRussianHetzer <sariego.nicolas@gmail.com>
2020-11-04 23:02:00 +0100

    Versión al español

--- BeanBash.sh
+++ BeanBash.sh
@@ -1,29 +1,120 @@
 #!/bin/bash
 
-# Check if the user is root
+# Revisa si el usuario es root
 if [ "$UID" -eq 0 ]
-# If the user is root, a message is displayed
-then echo "The script is running as sudo."
-# If the user is not root, request sudo permissions
-else echo "Sudo permissions are required to run the script."
+# Si el usuario es root, se muestra el mensaje
+then echo "El script se está ejecutando como administrador."
+# Si el usuario no es root, se solicitan permisos de administrador
+else echo "Se requieren permisos de administrador para ejecutar el script."
   exec sudo "$0" "$@"
+  # Fin de la condición
 fi
-# Stopping services and notifying the process
-echo "Stopping vmware service"
-service vmware stop
-echo "Stopping VMware Add-on Service"
-service vmware-USBArbitrator stop
-echo "Stopping epoptes service"
-service epoptes-client stop
-echo "Stopping SSH service"
-service ssh stop
-# Port blocking by ufw firewall
-echo "Activating firewall"
-ufw enable
-echo "Blocking epoptes port by firewall"
-ufw deny 789
-echo "Blocking SSH port by firewall"
-ufw deny 22/tcp
-echo "Killing remaining running processes"
-kill $(ps -ax | grep epoptes | cut -d" " -f2)
-kill $(ps -ax | grep epoptes-client | cut -d" " -f2)
+
+#!/bin/bash
+# Bash Menu Script Example
+echo "______                 ______           _     ";
+echo "| ___ \                | ___ \         | |    ";
+echo "| |_/ / ___  __ _ _ __ | |_/ / __ _ ___| |__  ";
+echo "| ___ \/ _ \/ _\` | '_ \| ___ \/ _\` / __| '_ \ ";
+echo "| |_/ /  __/ (_| | | | | |_/ / (_| \__ \ | | |";
+echo "\____/ \___|\__,_|_| |_\____/ \__,_|___/_| |_|";
+echo "                                              ";
+echo "                                              ";
+echo ""
+echo "Bienvenido a BeanBash. Desde este script podrás bloquear o activar epoptes y/o SSH."
+echo "Este script se distribulle sin ninguna garantía bajo licencia MIT. Copyright (c) 2020 TheRussianHetzer"
+echo "Puedes consultar el repositorio de este script en GitHub. https://github.com/TheRussianHetzer/BeanBash"
+echo "Elige la opción que desees realizar.";
+echo "A no ser que sea imprescindible el uso del SSH, recomendamos bloquearlo también. Así puedes evitar que vuelvan a activar epoptes de forma remota."
+echo ""
+echo ""
+PS3='Elige una opción: '
+options=("Bloquear epoptes" "Bloquear epoptes y SSH" "Activar epoptes" "Activar epoptes y SSH" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Bloquear epoptes")
+            echo "Deteniendo servicio vmware"
+            service vmware stop
+            echo "Deteniendo servicio complementario vmware"
+            service vmware-USBArbitrator stop
+            echo "Deteniendo servicio epoptes"
+            service epoptes-client stop
+            if sudo apt install ufw> /dev/null 2>&1 ; then
+              echo "El firewall ya se encuentra instalado, saltando instalación"
+            else
+              "Instalando firewall"
+              sudo apt install -y ufw 1> /dev/null
+            fi
+            echo "Activando firewall"
+            ufw enable
+            echo "Bloqueando puerto epoptes por firewall"
+            ufw deny 789
+            echo "Matando procesos en ejecución restantes"
+            kill $(ps -ax | grep epoptes | cut -d" " -f2) 2> /dev/null
+            kill $(ps -ax | grep epoptes-client | cut -d" " -f2) 2> /dev/null
+            echo "Proceso finalizado"
+            break
+            ;;
+        "Bloquear epoptes y SSH")
+            echo "Deteniendo servicio vmware"
+            service vmware stop
+            echo "Deteniendo servicio complementario vmware"
+            service vmware-USBArbitrator stop
+            echo "Deteniendo servicio epoptes"
+            service epoptes-client stop
+            echo "Deteniendo servicio SSH"
+            service ssh stop
+            if sudo apt install ufw> /dev/null 2>&1 ; then
+              echo "El firewall ya se encuentra instalado, saltando instalación"
+            else
+              "Instalando firewall"
+              sudo apt install -y ufw 1> /dev/null
+            fi
+            echo "Activando firewall"
+            ufw enable
+            echo "Bloqueando puerto epoptes por firewall"
+            ufw deny 789
+            echo "Bloqueando puerto SSH por firewall"
+            ufw deny 22/tcp
+            echo "Matando procesos en ejecución restantes"
+            kill $(ps -ax | grep epoptes | cut -d" " -f2) 2> /dev/null
+            kill $(ps -ax | grep epoptes-client | cut -d" " -f2) 2> /dev/null
+            echo "Proceso finalizado"
+            break
+            ;;
+        "Activar epoptes")
+            echo "Desbloqueando puerto epoptes por firewall"
+            ufw allow 789
+            echo "Activando servicio epoptes"
+            service epoptes-client start
+            echo "Activado servicio vmware"
+            service vmware start
+            echo "Activando servicio complementario vmware"
+            service vmware-USBArbitrator stop
+            echo "Proceso finalizado"
+            break
+            ;;
+        "Activar epoptes y SSH")
+        echo "Desbloqueando puerto epoptes por firewall"
+        ufw allow 789
+        echo "Desbloqueando puerto SSH por firewall"
+        ufw allow 22/tcp
+        echo "Activando servicio epoptes"
+        service epoptes-client start
+        echo "Activando servicio SSH"
+        service ssh start
+        echo "Activado servicio vmware"
+        service vmware start
+        echo "Activando servicio complementario vmware"
+        service vmware-USBArbitrator stop
+        echo "Proceso finalizado"
+        break
+            ;;
+        "Quit")
+        echo "¡Adiós!"
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
